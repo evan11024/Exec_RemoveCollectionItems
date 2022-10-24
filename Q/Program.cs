3db9d4296ac16212c0258ac5f1bd9a232f9e50ee
ispan <ispan@DESKTOP-PAKSETB>
2022-10-24 16:48:24 +0800

    加入專案檔案。

---
+++ Q/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<int> ints = new List<int>();
+			ints.Add(1);
+			ints.Add(2);
+			ints.Remove(1);
+			ints.Clear();
+
+			
+		}
+	}
+}
